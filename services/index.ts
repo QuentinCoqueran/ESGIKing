247879de1ece4f41d2c0dd928a2c212ae2867467
Mathieu <ferreira.mathieu.pro@gmail.com>
2022-05-22 17:21:03 +0200

    commit for merge

--- services/index.ts
+++ services/index.ts
@@ -4,3 +4,4 @@ export * from './restaurant.service';
 export * from './admin.service';
 export * from './order.service';
 export * from './menu.service';
+export * from './offer.service';
--- services/offer.service.ts
+++ services/offer.service.ts
@@ -14,32 +14,34 @@ export class OfferService {
         return OfferModel.find().exec();
     }
 
-    public async saveOffer(offer: Partial<OfferDocument>, menuToAdd: Pick<MenuProps, 'name'>[], productToAdd: Pick<ProductProps, 'name'>[]): Promise<OfferDocument> {
+    public async saveOffer(offer: Partial<OfferDocument>, menuToAdd?: Pick<MenuProps, 'name'>[], productToAdd?: Pick<ProductProps, 'name'>[]): Promise<OfferDocument> {
         let model = await OfferModel.findOne({name: offer.name});
         if(!model){
             model = await OfferModel.create({
                 name: offer.name,
                 discount: offer.discount
             });
-
-            for (let menu of menuToAdd) {
-                let menuModel = await MenuModel.findOne({name: menu.name});
-                if(menuModel){
-                    model.menuList.push(menuModel._id);
-                }else{
-                    throw new Error("Menu not found");
+            if(menuToAdd) {
+                for (let menu of menuToAdd) {
+                    let menuModel = await MenuModel.findOne({name: menu.name});
+                    if (menuModel) {
+                        model.menuList.push(menuModel._id);
+                    } else {
+                        throw new Error("Menu not found");
+                    }
                 }
             }
 
-            for (let product of productToAdd) {
-                let productModel = await ProductModel.findOne({name: product.name});
-                if(productModel){
-                    model.productList.push(productModel._id);
-                }else{
-                    throw new Error("Product not found");
+            if(productToAdd) {
+                for (let product of productToAdd) {
+                    let productModel = await ProductModel.findOne({name: product.name});
+                    if (productModel) {
+                        model.productList.push(productModel._id);
+                    } else {
+                        throw new Error("Product not found");
+                    }
                 }
             }
-
         }else{
             throw new Error("Offer already exist");
         }
